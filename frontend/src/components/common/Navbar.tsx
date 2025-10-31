fcd3a3bb903c962df2c289bb9e4c51ea830f7cc7
isak4307 <isak4307@gmail.com>
2025-10-31 13:09:06 +0100

    added logout in frontend

--- frontend/src/components/common/Navbar.tsx
+++ frontend/src/components/common/Navbar.tsx
@@ -3,7 +3,7 @@ import {useContext, useState} from "react";
 import {UserTokenContext} from "../../context/Context.tsx";
 
 export default function Navbar() {
-    const {userToken} = useContext(UserTokenContext);
+    const {userToken,setUserToken} = useContext(UserTokenContext);
     const [openUserMenu, setOpenUserMenu] = useState(false);
 
     return <nav aria-label="main navigation" className={"sticky top-0 pt-3 flex items-center justify-between"}>
@@ -43,7 +43,7 @@ export default function Navbar() {
                                 Profile
                             </NavLink>
                         </li>
-                        <li className={"hover:text-red-500"}>
+                        <li className={"hover:text-red-500"} onClick={()=> setUserToken({token:""})}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="size-6 logout-btn">
                                 <path fillRule="evenodd" d="M16.5 3.75a1.5 1.5 0 0 1 1.5 1.5v13.5a1.5 1.5 0 0 1-1.5 1.5h-6a1.5 1.5 0 0 1-1.5-1.5V15a.75.75 0 0 0-1.5 0v3.75a3 3 0 0 0 3 3h6a3 3 0 0 0 3-3V5.25a3 3 0 0 0-3-3h-6a3 3 0 0 0-3 3V9A.75.75 0 1 0 9 9V5.25a1.5 1.5 0 0 1 1.5-1.5h6ZM5.78 8.47a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 0 0 0 1.06l3 3a.75.75 0 0 0 1.06-1.06l-1.72-1.72H15a.75.75 0 0 0 0-1.5H4.06l1.72-1.72a.75.75 0 0 0 0-1.06Z" clipRule="evenodd" />
                             </svg>
